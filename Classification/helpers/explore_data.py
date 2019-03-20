af47bdaa6078192a7f6fda6d932b50f3a226834b
Yulia Zamriy <yulia.zamriy@gmail.com>
2019-03-19 23:03:38 -0400

    deleted test model

---
+++ Classification/helpers/explore_data.py
@@ -0,0 +1,93 @@
+"""Module to explore data.
+
+Contains functions to help study, visualize and understand datasets.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def get_num_words_per_sample(sample_texts):
+    """Gets the median number of words per sample given corpus.
+
+    # Arguments
+        sample_texts: list, sample texts.
+
+    # Returns
+        int, median number of words per sample.
+    """
+    num_words = [len(s.split()) for s in sample_texts]
+    return np.median(num_words)
+
+
+def plot_frequency_distribution_of_ngrams(sample_texts,
+                                          ngram_range=(1, 2),
+                                          num_ngrams=50):
+    """Plots the frequency distribution of n-grams.
+
+    # Arguments
+        samples_texts: list, sample texts.
+        ngram_range: tuple (min, mplt), The range of n-gram values to consider.
+            Min and mplt are the lower and upper bound values for the range.
+        num_ngrams: int, number of n-grams to plot.
+            Top `num_ngrams` frequent n-grams will be plotted.
+    """
+    # Create args required for vectorizing.
+    kwargs = {
+        'ngram_range': ngram_range,
+        'dtype': 'int32',
+        'strip_accents': 'unicode',
+        'decode_error': 'replace',
+        'analyzer': 'word',  # Split text into word tokens.
+    }
+    vectorizer = CountVectorizer(**kwargs)
+
+    # This creates a vocabulary (dict, where keys are n-grams and values are
+    # idxices). This also converts every text to an array the length of
+    # vocabulary, where every element idxicates the count of the n-gram
+    # corresponding at that idxex in vocabulary.
+    vectorized_texts = vectorizer.fit_transform(sample_texts)
+
+    # This is the list of all n-grams in the index order from the vocabulary.
+    all_ngrams = list(vectorizer.get_feature_names())
+    num_ngrams = min(num_ngrams, len(all_ngrams))
+    # ngrams = all_ngrams[:num_ngrams]
+
+    # Add up the counts per n-gram ie. column-wise
+    all_counts = vectorized_texts.sum(axis=0).tolist()[0]
+
+    # Sort n-grams and counts by frequency and get top `num_ngrams` ngrams.
+    all_counts, all_ngrams = zip(*[(c, n) for c, n in sorted(
+        zip(all_counts, all_ngrams), reverse=True)])
+    ngrams = list(all_ngrams)[:num_ngrams]
+    counts = list(all_counts)[:num_ngrams]
+
+    print(ngrams)
+
+    idx = np.arange(num_ngrams)
+    plt.figure(figsize=(20, 10))
+    plt.bar(idx, counts, width=0.8, color='b')
+    plt.xlabel('N-grams')
+    plt.ylabel('Frequencies')
+    plt.title('Frequency distribution of n-grams')
+    plt.xticks(idx, ngrams, rotation=90)
+    plt.show()
+
+
+def plot_sample_length_distribution(sample_texts):
+    """Plots the sample length distribution.
+
+    # Arguments
+        samples_texts: list, sample texts.
+    """
+    plt.hist([len(s) for s in sample_texts], 50)
+    plt.xlabel('Length of a speech')
+    plt.ylabel('Number of speeches')
+    plt.title('Speech length distribution')
+    plt.show()
